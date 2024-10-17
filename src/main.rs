c25213a69b9c713fcc9cb3072a0967151ca81ad0
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-10-17 15:32:58 +0530

    almost final changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 // to-do cli tool 
-use std::io::{self, Write};
-
-use clap::error;
-
+use std::io;
 
 static mut ALL_TASKS:Vec<Task> = Vec::new();
 
@@ -23,18 +20,27 @@ pub unsafe fn create_task(description: &str) {
     ALL_TASKS.push(task);
 }
 
-pub unsafe fn delete_task(id: u32){
-    ALL_TASKS.remove(id.try_into().unwrap());
+pub unsafe fn delete_task(id: u32) {
+    ALL_TASKS.remove((id - 1).try_into().unwrap());
 }
 
 pub unsafe fn print_all_tasks(){
+    if ALL_TASKS.len() == 0 {
+        println!("No tasks found! Please use command to add a new task.\n");
+    }
     for task in ALL_TASKS.iter(){
-        println!("Task: Id {} Description {} Completed: {}", task.id, task.description, task.completed);
+        println!("TaskId: {} \t Description: {} \t Completed: {}", task.id, task.description, task.completed);
     }
 }
 
-unsafe fn update_task(id : usize, task: Task){
-    ALL_TASKS[id] = task;
+unsafe fn update_task(id : u32, description: &str){
+    let mut iterator = ALL_TASKS.iter_mut(); 
+    while let Some(task) = iterator.next() {
+        if task.id == id {
+            task.description = description.to_string();
+            break;
+        }
+    }
 }
 
 fn print_introduction(){
@@ -46,17 +52,36 @@ fn print_introduction(){
     println!("6 -> Exit");
 }
 
-unsafe fn take_task_id_input() -> Result<u32, err> {
+unsafe fn take_task_id_input() -> Result<u32, i32> {
     // taking task ID as input
     println!("Please enter ID of the task: \n");
     let mut id_str = String::new();
     io::stdin().read_line(&mut id_str).expect("Error reading line");
     let id:u32 = id_str.trim().parse().expect("ID should be a number.");
-    if (id > ALL_TASKS.len()){
+    if id > ALL_TASKS.len().try_into().unwrap()  {
         println!("Plase enter a valid ID");
-        return error;
+        return Err(-1);
     }
-    return Result(id);
+    return Ok(id);
+}
+
+unsafe fn take_description_input() -> Result<String, i32> {
+    println!("Please enter a description for the task: \n");
+    let mut description = String::new();
+    io::stdin().read_line(&mut description).expect("Error reading input");
+    let description:String = description.trim().to_string();
+    return Ok(description);
+}
+
+unsafe fn complete_task(id: u32) -> Result<u32, i32> {
+    let mut iterator = ALL_TASKS.iter_mut();
+    while let Some(task) = iterator.next(){
+        if task.id == id {
+            task.completed = true;
+            break;
+        }
+    }
+    return Ok(0);
 }
 
 
@@ -70,57 +95,44 @@ fn main() {
 
         match choice {
             1 => unsafe {
-                println!("Please enter a description for the task: \n");
-                let mut description = String::new();
-                io::stdin().read_line(&mut description).expect("Error reading input");
-                let description:String = description.trim().to_string();
+                let description = take_description_input().unwrap();
                 create_task(&description);
             },
             2 => unsafe {
                 
                 // taking task ID as input
-                println!("Please enter ID of the task: \n");
-                let mut id_str = String::new();
-                io::stdin().read_line(&mut id_str).expect("Error reading line");
-                let id:u32 = id_str.trim().parse().expect("ID should be a number.");
-                if (id > ALL_TASKS.len()){
-                    println!("Plase enter a valid ID");
-                    continue;
-                }
+                let id = take_task_id_input().unwrap();
 
                 //taking new description as input
-                println!("Please enter the new description: \n");
-                let mut new_description = String::new();
-                io::stdin().read_line(&mut new_description).expect("Error reading line");
-                new_description = new_description.trim().to_string();
+                let description = take_description_input().unwrap();
 
                 //updating the task
-                update_task(id, task);
+                update_task(id, &description);
             },
-
             3 => unsafe {
                 // taking task ID as input
-                println!("Please enter ID of the task: \n");
-                let mut id_str = String::new();
-                io::stdin().read_line(&mut id_str).expect("Error reading line");
-                let id:u32 = id_str.trim().parse().expect("ID should be a number.");
-                if (id > ALL_TASKS.len()){
-                    println!("Plase enter a valid ID");
-                    continue;
-                }
-                ALL_TASKS[id].completed = true;
+                let id = take_task_id_input().unwrap();
+                complete_task(id).unwrap();
                 println!("Task marked as completed");
 
             }, 
-            4 => {
-
+            4 => unsafe {
+                let id = take_task_id_input().unwrap();
+                delete_task(id);
+            }, 
+            5 => unsafe {
+                print_all_tasks();
             }
             6 => 
             {
                 println!("Exiting...");
                 break;
             }
-            _ => {println!("nothing")}
+            _ => 
+            {
+                println!("Please enter a valid command.");
+                continue;
+            }
         }
     }
 }
